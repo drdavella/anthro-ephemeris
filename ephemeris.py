119d91dcc6a0921e32b8b55d7f89ccdea7b1a0f1
Daniel D'Avella <drdavella@gmail.com>
2018-04-23 13:40:51 -0400

    Implement basic flow for logging in to application

--- ephemeris.py
+++ ephemeris.py
@@ -1,14 +1,46 @@
 #!/usr/bin/env python
 import logging
 
-from flask import Flask, render_template, request, send_from_directory
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+from flask import (Flask, render_template, session, request, redirect, url_for)
 app = Flask('ephemeris', static_folder='static', static_url_path='')
+
+app.secret_key = 'is this secret key good enough?'
+
 
 @app.route('/')
 def welcome():
+    if 'user_id' in session:
+        logger.debug('user {} is already logged in'.format(session['user_id']))
+        return redirect(url_for('user_page'))
+
     return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def login():
-    logging.debug('LOGGING IN!')
-    return 'hey you logged in!: ' + str(request.form)
+    username = request.form['username']
+    password = request.form['password']
+
+    if username == 'ddavella' and password == 'password':
+        logger.debug('logging in as', username)
+        session['user_id'] = username
+        return redirect(url_for('user_page'))
+    else:
+        logger.debug('failed to log in')
+        return redirect(url_for('welcome'))
+
+@app.route('/logout')
+def logout():
+    if 'user_id' in session:
+        logger.debug('logging out {}'.format(session['user_id']))
+        session.pop('user_id')
+    return redirect(url_for('welcome'))
+
+@app.route('/user_page')
+def user_page():
+    if 'user_id' not in session:
+        return redirect(url_for('welcome'))
+
+    return 'welcome {}!'.format(session['user_id'])
